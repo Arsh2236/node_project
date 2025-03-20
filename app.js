e4d7493bd197ab1656e325cf976c76cbd2d8da37
Arsh2236 <36arshdeep@gmail.com>
2025-03-20 10:23:57 -0600

    update

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-// import the express package
 const express = require('express');
-require(`dotenv`).config();
+require('dotenv').config();
 const PORT = process.env.PORT;
-
-// initialize a new express instance equal to the app object
 const app = express();
 
-//route handler that sends back a response
-app.get(`/home`,(req, res) =>{
-    res.send("Hello, World");
+//setup our view engine
+app.set('view engine', 'ejs');
+app.set('views', "./views");
 
-});
-//roiute handler for a post request
-app.post('/user/:id', (req, res) =>{
-    const user_id = req.params.id;
-  res.send('This is post response in RESTFUL style');
-});
 
-//Start our server, listening on part 3000
-app.listen(PORT, () =>{
-    console.log(`Connected on port ${PORT}`);
+app.use(express.static('public'));
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Connected to port: ${PORT}`);
 });
