247484d19cdf20eac5b53f8dba41ba7249e1c46a
Arsh2236 <36arshdeep@gmail.com>
2025-03-26 11:25:38 -0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT;
+const userRoute = require('./routes/users.js');
 const app = express();
 
 //setup our view engine
@@ -9,8 +10,7 @@ app.set('views', "./views");
 
 
 app.use(express.static('public'));
-
-
+app.use(userRoute);
 
 app.listen(PORT, ()=>{
     console.log(`Connected to port: ${PORT}`);
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express =  require('express');
+const router = express.Router();
+
+
+
+
+//render our home page 
+router.get('/',(req,res)=>{
+res.render('home');
+});
+
+
+
+
+
+
+module.exports = router;
