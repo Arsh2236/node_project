bdc5b3b4f0c65167f171f18491eda1e1b285a996
Arsh2236 <36arshdeep@gmail.com>
2025-04-01 11:18:32 -0600

    Update

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
-const express =  require('express');
+
+const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController.js');
 
 
 
-
-//render our home page 
-router.get('/',(req,res)=>{
-res.render('home');
+//render our home page
+router.get('/', (req, res)=>{
+    res.render('home');
 });
 
-
-
-
+//post route handler to create a new user
+router.post('/users', userController.createUser);
 
 
 module.exports = router;
